7b10b9930370b63a7f9cc1c0226d2b1e0c47b167
Jaesang Won <77087336+wonjaesang@users.noreply.github.com>
2021-05-22 17:37:52 +0900

    Add files via upload

--- js/start.js
+++ js/start.js
@@ -1,10 +1,11 @@
 const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
-
+const endPoint = 12;
 
 function addAnswer(answerText, qIdx) {
   var a = document.querySelector(".answerBox");
-  var answer = document.createElement('Button');
+  var answer = document.createElement('button');
+  answer.classList.add('answerList');
   answer.classList.add('my-3');
   answer.classList.add('py-3');
   answer.classList.add('mx-auto');
@@ -34,6 +35,8 @@ function goNext(qIdx){
   for(let i in qnaList[qIdx].a){
     addAnswer(qnaList[qIdx].a[i].answer, qIdx);
   }
+  var status = document.querySelector(".statusBar");
+  status.style.width = (100/endPoint) * (qIdx+1) +'%';
 }
 
 function begin() {
