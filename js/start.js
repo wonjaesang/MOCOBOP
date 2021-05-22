6979abbd0fa497f7e17fd5c50b1d0c06e8531593
Jaesang Won <77087336+wonjaesang@users.noreply.github.com>
2021-05-22 17:24:35 +0900

    Add files via upload

--- js/start.js
+++ js/start.js
@@ -2,18 +2,37 @@ const main = document.querySelector("#main");
 const qna = document.querySelector("#qna");
 
 
-function addAnswer(answerText) {
+function addAnswer(answerText, qIdx) {
   var a = document.querySelector(".answerBox");
   var answer = document.createElement('Button');
+  answer.classList.add('my-3');
+  answer.classList.add('py-3');
+  answer.classList.add('mx-auto');
+  answer.classList.add('fadeIn');
+
   a.appendChild(answer);
   answer.innerHTML = answerText;
+  answer.addEventListener("click", function(){
+    var children = document.querySelectorAll('.answerList');
+    for(let i = 0; i < children.length; i++){
+      children[i].disabled = true;
+      children[i].style.WebkitAnimation = "fadeOut 0.5s";
+      children[i].style.animation = "fadeOut 0.5s";
+    }
+    setTimeout(() => {
+      for(let i = 0; i < children.length; i++){
+        children[i].style.display = 'none';
+      }
+      goNext(++qIdx);
+    }, 450)
+  }, false);
 }
 
-function goNext(qIdx) {
+function goNext(qIdx){
   var q = document.querySelector(".qBox");
   q.innerHTML = qnaList[qIdx].q;
   for(let i in qnaList[qIdx].a){
-    addAnswer(qnaList[qIdx].a[i].answer);
+    addAnswer(qnaList[qIdx].a[i].answer, qIdx);
   }
 }
 
